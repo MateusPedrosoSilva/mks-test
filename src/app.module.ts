a9ec77815f221f9255bd6b5e3e469fb63df2928d
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-05-30 02:43:23 -0300

    feat(typeorm): Add Typeorm connection with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'pg_db',
+      port: 5432,
+      database: 'mks_movies_db',
+      username: 'mksUser',
+      password: 'MksPass123',
+      synchronize: true
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
