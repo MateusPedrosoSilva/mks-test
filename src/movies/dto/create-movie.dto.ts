e69b95bf84a7942db76e1c782e4e0649e72c61f3
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-06-02 11:38:02 -0300

    feat(documentation): add description to the DTOs of movies

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,15 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
+  @ApiProperty({ description: 'Name of the movie', default: 'Matrix' })
   name: string;
 
   @IsString()
+  @ApiProperty({ description: 'Genre od the movie', default: 'Fiction' })
   genre: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'Director of the movie',
+    default: 'Peter Jackson',
+  })
   director: string;
 
   @IsNumber()
+  @ApiProperty({ description: 'Year of the movie', default: '2000' })
   year: number;
 }
